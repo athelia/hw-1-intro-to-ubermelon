a72f9b1aab3b2b8282530e7524387ee3fce25faa
Athelia Crosmun <hi@athelia.codes>
2021-03-05 20:12:30 +0000

    Add first comment

--- process.py
+++ process.py
@@ -1,8 +1,8 @@
+# Opens a file and saves the output to log_file var
 log_file = open("um-server-01.txt")
 
-
-def sales_reports(log_file):
-    for line in log_file:
+def sales_reports(log):
+    for line in log:
         line = line.rstrip()
         day = line[0:3]
         if day == "Tue":
